ecd9be7980fe5464a104edad56006e404df9855a
SvOlga <sviridova-olga.iccf@yandex.ru>
2020-05-21 12:57:57 +0300

    First commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int percent = 5;
+        int ticketPrice = 5500;
+        int miles = ticketPrice * percent / 100;
+        System.out.println(miles);
+    }
+}
